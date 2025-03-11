6150e994d556b0bb20663d70480a0683627fda2b
Md Masud karim <msmasud578@gmail.com>
2025-03-11 15:03:49 +0100

    UPDATED the CoordMatrix class

--- src/CoordMatrix.py
+++ src/CoordMatrix.py
@@ -2,10 +2,11 @@ import numpy as np
 
 '''
 <h1>CoordMatrix</h1>
-This class is responsible for handling matrix operations.
+This class represents a matrix (physical form) aka matrix defination.
+It is capable of performing some regular operations like shearing, rotation, mirroring and scaling and transposition.
 '''
 class CoordMatrix:
-    def __init__(self, shape = (50,50), x_range = 50, y_range = 50, value_to_character_mapper:dict = {}):
+    def __init__(self, shape = (50,50,50), x_range = 50, y_range = 50, z_range = 50 ,value_to_character_mapper:dict = {}):
         self.matrix = np.zeros(shape)
         self.x_range = x_range
         self.y_range = y_range
@@ -15,13 +16,8 @@ class CoordMatrix:
         
     def __eq__(self, value):
         return self.matrix == value.matrix
-    
-    def __add__(self, value):
-        return self.matrix + value.matrix
-    def __sub__(self, value):
-        return self.matrix - value.matrix
-    def __mul__(self, value):
-        return self.matrix * value.matrix
+
+
     def get_transpose(self):
         return self.matrix.T
     def get_inverse(self):
--- src/Display.py
+++ src/Display.py
@@ -1,4 +1,8 @@
 import curses
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 class Display(object):
     def __init__(self):
         curses.initscr()
+    def start_main_window(self):
+        pass
