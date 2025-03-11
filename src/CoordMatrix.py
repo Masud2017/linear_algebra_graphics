3038cf47af5249d10f408e2764e52ad7c1ac79f8
Md Masud karim <msmasud578@gmail.com>
2025-03-11 03:29:28 +0100

    Renamed Matrix to CoordMatrix

--- src/CoordMatrix.py
+++ src/CoordMatrix.py
@@ -1,12 +1,17 @@
 import numpy as np
 
 '''
-<h1>Matrix</h1>
+<h1>CoordMatrix</h1>
 This class is responsible for handling matrix operations.
 '''
-class Matrix:
-    def __init__(self, shape = (150,150)):
+class CoordMatrix:
+    def __init__(self, shape = (50,50), x_range = 50, y_range = 50, value_to_character_mapper:dict = {}):
         self.matrix = np.zeros(shape)
+        self.x_range = x_range
+        self.y_range = y_range
+        
+        print(f"Printign the value of the matrix")
+        print(self.matrix)
         
     def __eq__(self, value):
         return self.matrix == value.matrix
@@ -24,4 +29,7 @@ class Matrix:
     def get_determinant(self):
         return np.linalg.det(self.matrix)
 
+    
+if __name__ == "__main__":
+    matrix = CoordMatrix()
     
