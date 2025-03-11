8902614ff1ae05bf89b995fbfb9bdc5cd50d38c3
Md Masud karim <msmasud578@gmail.com>
2025-03-11 03:40:34 +0100

    added src/obj_parser submodule

---
+++ src/ObjParser.py
@@ -0,0 +1,15 @@
+from CoordMatrix import CoordMatrix
+
+class ObjParser(object):
+    def __init__(self,filename):
+        self.filename = filename
+        self.obj_file = open(filename, "r")
+    
+    def _parse_header(self):
+        
+        pass
+    def _parse_body(self):
+        pass
+    
+    def get_coord_matrix(self) -> CoordMatrix:
+        pass
